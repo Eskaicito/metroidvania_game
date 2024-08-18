201c8400c5f077590d98f2fa539e89e28791738a
SatoruuGoyoo <mentabullrichgregorio@gmail.com>
2024-08-18 17:35:38 -0300

    Fail

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject pauseMenuCanvas;
+    public GameObject pauseMenuInstance;
+    public GameObject pauseMenuPrefab;
     private bool isPaused = false;
 
-   
+    void Start()
+    {
+        
+        if (pauseMenuInstance == null)
+        {
+            pauseMenuInstance = Instantiate(pauseMenuPrefab);
+            DontDestroyOnLoad(pauseMenuInstance);
+            pauseMenuInstance.SetActive(false); 
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -26,7 +38,7 @@ public class PauseMenu : MonoBehaviour
 
     public void ResumeGame()
     {
-        pauseMenuCanvas.SetActive(false);
+        pauseMenuInstance.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
         
@@ -34,7 +46,7 @@ public class PauseMenu : MonoBehaviour
 
     public void PauseGame()
     {
-        pauseMenuCanvas.SetActive(true);
+        pauseMenuInstance.SetActive(true);
         Time.timeScale = 0;
         isPaused = true;
     }
@@ -42,5 +54,28 @@ public class PauseMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    void OnEnable()
+    {
+        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        
+        if (pauseMenuInstance == null)
+        {
+            pauseMenuInstance = Instantiate(pauseMenuPrefab);
+            DontDestroyOnLoad(pauseMenuInstance);
+            pauseMenuInstance.SetActive(false);
+        }
     }
 }
