e1ff444487d17be7899bcacd16fc2c3417412be4
SatoruuGoyoo <mentabullrichgregorio@gmail.com>
2024-09-16 10:49:43 -0300

    hola

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health;
-    [SerializeField] private float detectionRange = 10f;
-    [SerializeField] bool isPlayerInRange;
-
-    [SerializeField] public Transform player;
-    public Transform Player => player; // Propiedad
-
 
     [SerializeField] float speed = 3f;
     public float Speed => speed; // Propiedad
@@ -20,44 +14,7 @@ public class Enemy : MonoBehaviour
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
-
-
-    public virtual void DetectPlayer()
-    {
-        // Dirección hacia el jugador
-        Vector3 directionToPlayer = (player.position - transform.position).normalized;
-
-        // Crear un raycast desde la posición del enemigo en la dirección del jugador
-        RaycastHit hit;
-
-        // Si el rayo impacta algo dentro del rango de detección
-        if (Physics.Raycast(transform.position, directionToPlayer, out hit, detectionRange))
-        {
-            // Si el rayo golpea al jugador
-            if (hit.collider.CompareTag("Player")) // Asegúrate de que el jugador tiene la etiqueta "Player"
-            {
-                isPlayerInRange = true;
-            }
-            else
-            {
-                isPlayerInRange = false;
-            }
-        }
-        else
-        {
-            isPlayerInRange = false;
-        }
-    }
-
-    public void Update()
-    {
-        DetectPlayer();
-        if (isPlayerInRange)
-        {
-            Move();
-        }
+       
     }
 
     public void TakeDamage(float damage)
@@ -83,9 +40,6 @@ public class Enemy : MonoBehaviour
         Destroy(gameObject);
     }
 
-    public virtual void Move()
-    {
-
-    }
+   
 
 }
--- Assets/Scripts/Enemy/PatrollingEnemy.cs
+++ Assets/Scripts/Enemy/PatrollingEnemy.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class PatrollingEnemy : Enemy
 {
-    public override void Move()
-    {
-        // Seguir al jugador solo en el plano XZ (terrestre)
-        Vector3 direction = (Player.position - transform.position).normalized;
-        direction.y = 0; // Aseguramos que no suba o baje, solo en XZ
-
-        transform.position += direction * Speed * Time.deltaTime;
-
-        // Opcional: Rotar para que mire hacia el jugador
-        transform.LookAt(new Vector3(Player.position.x, transform.position.y, Player.position.z));
-    }
+  
 }
