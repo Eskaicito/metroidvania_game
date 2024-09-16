c0996844a9c53beb403a3e279193f2e17f4a8511
Sebarbosa10 <sebastianbarbosa10@gmail.com>
2024-09-16 11:56:24 -0300

    barra de vida

---
+++ Assets/Scripts/Player/HealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthBar;
+
+    public float ActualHealth;
+    public float HealthMax;
+
+  
+
+    
+    void Update()
+    {
+        healthBar.fillAmount = ActualHealth / HealthMax;
+
+    }
+}
